87a23b8c67015db753a5f848ee07b776a453b584
Sanjim Hasan <sanjimhasan911@gmail.com>
2024-07-16 14:18:22 +0600

    Add files via upload

--- scripts/5crip7_1.js
+++ scripts/5crip7_1.js
@@ -23,3 +23,29 @@ var formattedTime = hours + ':' + minutes + ' ' + period;
 
 // Display in the swapped format with the year and time
 document.getElementById('last-updated').textContent = "Updated: " + month + " " + date + ", " + year + " at " + formattedTime;
+
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const toggle = document.getElementById('darkmode-toggle');
+
+    // Check for saved theme in localStorage
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        document.body.classList.add(savedTheme);
+        if (savedTheme === 'darkTheme') {
+            toggle.checked = true;
+        }
+    }
+
+    // Listen for toggle change
+    toggle.addEventListener('change', function() {
+        if (this.checked) {
+            document.body.classList.add('darkTheme');
+            localStorage.setItem('theme', 'darkTheme');
+        } else {
+            document.body.classList.remove('darkTheme');
+            localStorage.setItem('theme', 'lightTheme');
+        }
+    });
+});
